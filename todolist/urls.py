fbb1870b8c7807e82f62b2dacd5a5823a1dc41b0
Vadim Gera <horsespb@yandex.ru>
2024-06-25 00:04:30 +0300

    Первый Django проект

---
+++ todolist/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import base, about
+
+
+urlpatterns = [
+    path('', base, name='base'),
+    path('about', about, name='base'),
+]
---
+++ todolist/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def base(request):
+    return render(request, 'todolist/base.html')
+
+
+def about(request):
+    return render(request, 'todolist/about.html')
+
